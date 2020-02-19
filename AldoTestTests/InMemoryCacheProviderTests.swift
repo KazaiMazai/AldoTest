b50574f02bd5cc229d95045b2fb25aa6c23983da
Sergey Kazakov <kazaimazai@gmail.com>
2020-02-19 17:22:08 +0300

    added simple tests for in-mem cache

---
+++ AldoTestTests/InMemoryCacheProviderTests.swift
@@ -0,0 +1,9 @@
+//
+//  InMemoryCacheProviderTests.swift
+//  AldoTestTests
+//
+//  Created by Sergey Kazakov on 19/02/2020.
+//  Copyright © 2020 Piyush Sharma. All rights reserved.
+//
+
+import Foundation
